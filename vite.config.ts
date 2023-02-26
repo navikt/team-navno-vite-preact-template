aa97e89f9479c5e3412da4f2ae75520d3c2c0928
Anders Bråthen Nomerstad <anders.brathen.nomerstad@nav.no>
2023-02-26 18:18:13 +0100

    Bundle analyzer + sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig, loadEnv } from 'vite';
 import preact from '@preact/preset-vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
     return {
-        plugins: [preact()],
+        plugins: [
+            preact(),
+            ...(process.env.ANALYZE
+                ? [visualizer({ gzipSize: true, open: true, sourcemap: true })]
+                : []),
+        ],
+        build: {
+            sourcemap: true,
+        },
         ssr: {
             // Dependencies containing React components must not be externalized
             // in order to work with preact/compat. This list must also include
